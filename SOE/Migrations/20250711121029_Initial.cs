4a7e48a91ef0f712638b466ff54f468df4352185
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2025-07-11 09:13:56 -0300

    Add initial migration

---
+++ SOE/Migrations/20250711121029_Initial.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Elections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Options",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ElectionId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Options", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Options_Elections_ElectionId",
+                        column: x => x.ElectionId,
+                        principalTable: "Elections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    ExpirationUtc = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    VoterId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Used = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Voters_VoterId",
+                        column: x => x.VoterId,
+                        principalTable: "Voters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VoterElections",
+                columns: table => new
+                {
+                    VoterId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ElectionId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    VoterPublicKey = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    VoterSignature = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    OptionId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    VoteTimeUtc = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VoterElections", x => new { x.VoterId, x.ElectionId });
+                    table.ForeignKey(
+                        name: "FK_VoterElections_Elections_ElectionId",
+                        column: x => x.ElectionId,
+                        principalTable: "Elections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VoterElections_Options_OptionId",
+                        column: x => x.OptionId,
+                        principalTable: "Options",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VoterElections_Voters_VoterId",
+                        column: x => x.VoterId,
+                        principalTable: "Voters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Options_ElectionId",
+                table: "Options",
+                column: "ElectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_VoterId",
+                table: "Tokens",
+                column: "VoterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VoterElections_ElectionId",
+                table: "VoterElections",
+                column: "ElectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VoterElections_OptionId",
+                table: "VoterElections",
+                column: "OptionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tokens");
+
+            migrationBuilder.DropTable(
+                name: "VoterElections");
+
+            migrationBuilder.DropTable(
+                name: "Options");
+
+            migrationBuilder.DropTable(
+                name: "Voters");
+
+            migrationBuilder.DropTable(
+                name: "Elections");
+        }
+    }
+}
