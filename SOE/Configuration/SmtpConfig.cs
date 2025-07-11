6fa8c9a7343c8c03cc871cf0a2a6b5861e384c41
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2025-07-11 08:25:45 -0300

    Add missing SmtpConfig

---
+++ SOE/Configuration/SmtpConfig.cs
@@ -0,0 +1,11 @@
+namespace SOE.Configuration;
+
+public class SmtpConfig {
+    
+    public string From { get; set; } = string.Empty;
+    
+    public string Server { get; set; } = string.Empty;
+    
+    public string Password { get; set; } = string.Empty;
+    
+}
